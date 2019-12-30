2559af6e39a3a81d92ee2157f015f9c89ef26e47
LKHZ <leta6603@gmail.com>
2019-12-30 17:02:05 +0900

    Initial Commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by 이경훈 on 2019/12/30.
+//  Copyright © 2019 이경훈. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
